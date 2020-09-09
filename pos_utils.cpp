7b24f1ab0bf99e8f705bc790a3b613dc36a0d734
reyalsyssup <63643459+reyalsyssup@users.noreply.github.com>
2020-09-09 14:09:06 +1000

    Defined new static functions for vector2

--- pos_utils.cpp
+++ pos_utils.cpp
@@ -1,4 +1,5 @@
 #include "pos_utils.h"
+#include "math.h"
 
 using namespace plib;
 
@@ -15,6 +16,23 @@ Vector3::Vector3(double x, double y, double z) {
 }
 
 // Vector 2
+
+// functions
+double Vector2::getXDist(Vector2 point1, Vector2 point2) {
+    double dist = point1.x - point2.x;
+    return dist < 0 ? dist *= -1 : dist;
+}
+
+double Vector2::getLength(Vector2 point1, Vector2 point2) {
+    double length = 0.0;
+    double run = point1.x - point2.x;
+    double rise = point1.y - point2.y;
+    run = run > 0 ? run *= -1 : run;
+    rise = rise > 0 ? rise *= -1 : rise;
+    return sqrt(run*run + rise*rise);
+}
+
+// operators
 
 Vector2 Vector2::operator + (Vector2 vector) {
     Vector2 newVector(0, 0);
