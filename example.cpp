23a6f16162bd2d8ff99b9ce8273f012731365617
reyalsyssup <63643459+reyalsyssup@users.noreply.github.com>
2020-09-09 14:10:02 +1000

    Updated example to reflect prior changes

--- example.cpp
+++ example.cpp
@@ -3,8 +3,8 @@
 #include "pos_utils.h"
 
 int main() {
-    plib::Vector2 point1(5, 5);
-    plib::Vector2 point2(10, 10);
+    plib::Vector2 point1(10, 10);
+    plib::Vector2 point2(5, 5);
     plib::Vector2 point3 = point1 + point2;
     plib::Vector2 point4 = point1 - point2;
     plib::Vector2 point5(2, 2);
@@ -26,4 +26,6 @@ int main() {
 
     if(point1 == point2) std::cout << "Point 1 is the same as point 2" << std::endl;
     if(point7 != point6 && point7 == point3) std::cout << "Point 7 is the same as point 3 but different to point 6" << std::endl;
+    std::cout << "Distance from point1 to point2: " << plib::Vector2::getLength(point1, point2) << std::endl;
+    std::cout << "Distance from point1 to point2 on the x axis: " << plib::Vector2::getXDist(point1, point2) << std::endl;
 }
