9ad7d45ad9926fa41d0f8f9a86676b22c33c6f93
reyalsyssup <63643459+reyalsyssup@users.noreply.github.com>
2020-09-09 11:26:39 +1000

    Added Vector3 to implementation file

--- pos_utils.cpp
+++ pos_utils.cpp
@@ -34,3 +34,42 @@ Vector2 Vector2::operator -= (Vector2 vector) {
     newVector.y = y -= vector.y;
     return newVector;
 }
+
+
+
+Vector3 Vector3::operator + (Vector3 vector) {
+    Vector3 newVector;
+    newVector.x = x + vector.x;
+    newVector.y = y + vector.y;
+    newVector.z = z + vector.z;
+    return newVector;
+}
+Vector3 Vector3::operator - (Vector3 vector) {
+    Vector3 newVector;
+    newVector.x = x - vector.x;
+    newVector.y = y - vector.y;
+    newVector.z = z - vector.z;
+    return newVector;
+}
+
+bool Vector3::operator == (Vector3 vector) {
+    return x == vector.x && y == vector.y && z == vector.z;
+}
+bool Vector3::operator != (Vector3 vector) {
+    return x != vector.x && y != vector.y && vector.z != z;
+}
+
+Vector3 Vector3::operator += (Vector3 vector) {
+    Vector3 newVector;
+    newVector.x = x += vector.x;
+    newVector.y = y += vector.y;
+    newVector.z = z += vector.z;
+    return newVector;
+}
+Vector3 Vector3::operator -= (Vector3 vector) {
+    Vector3 newVector;
+    newVector.x = x -= vector.x;
+    newVector.y = y -= vector.y;
+    newVector.z = z -= vector.z;
+    return newVector;
+}
